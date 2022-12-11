728e20b0a75fcdfbacbb1c55cc6b0cbae0e8f1d6
Alexei <gimp24@mail.ru>
2022-12-11 14:09:10 +0300

    Из проекта удалены лишние файлы;

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NursaceMVC.Models;
 using System.Diagnostics;
 
 namespace NursaceMVC.Controllers
@@ -16,17 +15,6 @@ namespace NursaceMVC.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
