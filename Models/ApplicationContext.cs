c8475df3cf8da8c1321598cb1d4bb7d19e1d047f
Alexei <gimp24@mail.ru>
2022-12-16 23:56:10 +0300

    Добавлено: -  контекст данных ApplicationContext; -  настройка подключения к бд в appsettings; - сервис подключения к бд в Program; -  метод вывода пользователей и добавления - в контроллер;

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NursaceMVC.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<OrderString> OrderStrings { get; set; }
+        public DbSet<OrderList> OrderLists { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+
+    }
+}
