75624b683b2570ade9b4adf4587a47eaedc6e131
Alexei <gimp24@mail.ru>
2022-12-17 18:57:42 +0300

    Из Home котроллера вынесены методы управления сущностью клиента в Client котроллера; Так же была созданна отдельная папка для представлений Client;

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NursaceMVC.Models;
+
+namespace NursaceMVC.Controllers
+{
+    public class ClientController : Controller
+    {
+        ApplicationContext db;
+        public ClientController(ApplicationContext context)
+        {
+            db = context;
+        }
+
+        /// <summary>
+        /// Страница с формой добавления клиента
+        /// </summary>
+        public IActionResult CreateClient()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// POST метод добавления клиента
+        /// </summary>
+        /// <param name="client">обьект пользователя, который создаем</param>
+        [HttpPost]
+        public async Task<IActionResult> CreateClient(Client client)
+        {
+            db.Clients.Add(client);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>
+        /// Страница с формой редактирования клиента
+        /// </summary>
+        /// <param name="id">id пользователя который будем редактировать</param>
+        public async Task<IActionResult> EditClient(int? id)
+        {
+            if (id != null)
+            {
+                Client? client = await db.Clients.FirstOrDefaultAsync(c => c.Id == id);
+                if (client != null)
+                    return View(client);
+            }
+            return NotFound();
+        }
+
+        /// <summary>
+        /// POST метод редактирования клиента
+        /// </summary>
+        /// <param name="client">обьект пользователя, которого отредактировали</param>
+        [HttpPost]
+        public async Task<IActionResult> EditClient(Client client)
+        {
+            db.Clients.Update(client);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        /// <summary>
+        /// POST метод удаления клиента
+        /// </summary>
+        /// <param name="id">id пользователя</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> DeleteClient(int? id)
+        {
+            if (id != null)
+            {
+                Client client = new Client { Id = id.Value };
+                db.Entry(client).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return NotFound();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,54 +13,14 @@ namespace NursaceMVC.Controllers
             db = context;
         }
 
+        /// <summary>
+        /// Главная страница со списком пользователей
+        /// </summary>
+        /// <returns>Список пользователей</returns>
         public async Task<IActionResult> Index()
         {
             return View(await db.Clients.ToListAsync());
         }
 
-        public IActionResult CreateClient()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateClient(Client client)
-        {
-            db.Clients.Add(client);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        public async Task<IActionResult> EditClient(int? id)
-        {
-            if(id != null)
-            {
-                Client? client = await db.Clients.FirstOrDefaultAsync(c => c.Id == id);
-                if (client != null)
-                    return View(client);
-            }
-            return NotFound();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> EditClient(Client client)
-        {
-            db.Clients.Update(client);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> DeleteClient(int? id)
-        {
-            if (id != null)
-            {
-                Client client = new Client { Id = id.Value };
-                db.Entry(client).State = EntityState.Deleted;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return NotFound();
-        }
     }
 }
