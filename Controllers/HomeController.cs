a503c80de535b8f184467d873856d2450aca0589
Alexei <gimp24@mail.ru>
2022-12-17 18:27:45 +0300

    В контроллер добавлен метод изменения и удаления клиента; Добавлена страница для редактирования записи клиента;

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,11 +24,43 @@ namespace NursaceMVC.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Client client)
+        public async Task<IActionResult> CreateClient(Client client)
         {
             db.Clients.Add(client);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> EditClient(int? id)
+        {
+            if(id != null)
+            {
+                Client? client = await db.Clients.FirstOrDefaultAsync(c => c.Id == id);
+                if (client != null)
+                    return View(client);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditClient(Client client)
+        {
+            db.Clients.Update(client);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteClient(int? id)
+        {
+            if (id != null)
+            {
+                Client client = new Client { Id = id.Value };
+                db.Entry(client).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
         }
     }
 }
